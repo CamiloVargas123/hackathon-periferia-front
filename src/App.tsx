4fd9cbb859171d671d38bf266ab26af8931e1b39
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-07 02:32:10 -0500

    capture sequencie for valid

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import { Box, Button, Container, HStack, Input, VStack } from "@chakra-ui/react"
-
+import { Box, Button, Container, FormControl, FormErrorMessage, FormHelperText, HStack, Input, VStack } from "@chakra-ui/react"
+import { useCallback, useEffect, useRef, useState } from "react"
+import debounce from 'just-debounce-it'
+import { useMutant } from "./hooks/useMutant"
 
 function App() {
-
+  const { ADN, updateADN, error } = useValid()
+  const { validMutant } = useMutant({ ADN })
+  function submit(e: React.FormEvent) {
+    e.preventDefault()
+  }
+  const debouncedValidSequencie = useCallback(
+    debounce((ADN: string) => {
+      validMutant({ adn: ADN.split(",") })
+    }, 500)
+    , [validMutant]
+  )
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearch = event.target.value
+    updateADN(newSearch.toUpperCase())
+    debouncedValidSequencie(newSearch)
+  }
   return (
     <Box as="main" h={"max-content"}>
       <Container maxW='2xl' centerContent paddingBlock={10}>
-        <VStack as="form" gap={10} >
-          <HStack>
-            <Input placeholder="Ingrese la secuencia de ADN" id="input"  />
+        <FormControl display={"flex"} flexDir={"column"} gap={10} onSubmit={submit} isInvalid={!!error}>
+          <HStack alignItems={"start"}>
+            <VStack gap={1} w="100%">
+              <Input placeholder="Ingrese la secuencia de ADN" id="input" onChange={handleChange} />
+              {error ? <FormErrorMessage>{error}</FormErrorMessage> :
+                <FormHelperText color={"white"}>
+                  Ingresa una cadena de ADN separada por comas. Ejemplo: ATGCGA,CAGTGC,TTATGT,AGAAGG,CCCCTA,TCACTG
+                </FormHelperText>
+              }
+            </VStack>
             <Button color={"primary"} bgColor={"bg.500"} px={6} type="submit">Es mutante?</Button>
           </HStack>
           <Box >asd</Box>
-        </VStack>
+        </FormControl>
       </Container>
     </Box>
   )
@@ -21,3 +45,24 @@ function App() {
 export default App
 
 
+function useValid() {
+  const [ADN, updateADN] = useState<string>('')
+  const [error, setError] = useState<string>()
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = ADN === ''
+      return
+    }
+
+    if (ADN === '') {
+      setError('No se puede buscar una secuencia de ADN vacía')
+      return
+    }
+
+    setError(undefined)
+  }, [ADN])
+
+  return { ADN, updateADN, error }
+}

89260f87c19e01871964dd485a3f90f6b3ee43ae
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-07 02:59:50 -0500

    create custom hook for valid mutant

---
+++ src/hooks/useMutant.ts
@@ -0,0 +1,20 @@
+import { useCallback, useRef, useState } from 'react'
+import { validSequencie } from '../services/validSequencie.js'
+
+export function useMutant({ ADN }: { ADN: string }) {
+  const [isMutant, setIsMutant] = useState(false)
+  const previousADN = useRef(ADN)
+
+  const validMutant = useCallback(({ ADN }: { ADN: string }) => {
+    if (ADN === previousADN.current) return
+
+    try {
+      previousADN.current = ADN
+      setIsMutant(validSequencie({ adn: ADN.split(",") }))
+    } catch (e) {
+      console.log(e)
+    }
+
+  }, [])
+  return { validMutant, isMutant }
+}

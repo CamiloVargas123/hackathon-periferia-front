e5f24c99ed2e3d7a1bae9729961169bd38a25492
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-08 07:05:03 -0500

    apply corrections

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useMutant } from "./hooks/useMutant"
 
 function App() {
   const { ADN, updateADN, error } = useValid()
-  const { validMutant, isMutant } = useMutant({ ADN })
+  const { validMutant, isMutant, isLoading } = useMutant({ ADN })
 
   const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
     e.preventDefault()
@@ -13,46 +13,45 @@ function App() {
   }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.includes('"')) return
     const newSearch = event.target.value.toUpperCase()
     updateADN(newSearch)
   }
   return (
     <Box as="main" h={"max-content"}>
-      <Container maxW='2xl' centerContent paddingBlock={10}>
-        <VStack gap={10} >
-          <FormControl as="form" display={"flex"} alignItems={"start"} gap={10} isInvalid={!!error} onSubmit={handleSubmit}>
-            <VStack gap={0} w="100%">
-              <Input placeholder="Ingrese la secuencia de ADN" value={ADN} onChange={handleChange} />
-              {error ? <FormErrorMessage>{error}</FormErrorMessage> :
-                <FormHelperText color={"white"}>
-                  Ingresa una cadena de ADN separada por comas. Ejemplo: ATGCGA,CAGTGC,TTATGT,AGAAGG,CCCCTA,TCACTG
-                </FormHelperText>
-              }
-            </VStack>
-            <Button color={"primary"} bgColor={"bg.500"} px={6} type="submit">Es mutante?</Button>
-          </FormControl>
-          {
-            isMutant && <Alert
-              status='success'
-              variant='subtle'
-              flexDirection='column'
-              alignItems='center'
-              justifyContent='center'
-              textAlign='center'
-              height='200px'
-              bgColor={"#8fa800"}
-            >
-              <AlertIcon boxSize='40px' mr={0} color={"white"} />
-              <AlertTitle mt={4} mb={1} fontSize='lg' color={"bg.500"}>
-                Estudiante preparado para luchar!
-              </AlertTitle>
-              <AlertDescription maxWidth='sm' color={"bg.500"}>
-                La secuencia de ADN coincide con más de 4 letras iguales en sus diferentes formar (oblicua, horizontal o vertical)
-              </AlertDescription>
-            </Alert>
-          }
-          <ConsultStat />
-        </VStack>
+      <Container maxW='2xl' justifyContent={"space-between"} centerContent paddingBlock={10} gap={10}>
+        <FormControl as="form" display={"flex"} alignItems={"start"} gap={10} isInvalid={!!error} onSubmit={handleSubmit} isDisabled={isLoading || isMutant}>
+          <VStack gap={0} w="100%" alignItems={"flex-start"}>
+            <Input placeholder="Ingrese la secuencia de ADN" value={ADN} onChange={handleChange} />
+            {error ? <FormErrorMessage>{error}</FormErrorMessage> :
+              <FormHelperText color={"white"}>
+                Ejemplo: ATGCGA,CAGTGC,TTATGT,AGAAGG,CCCCTA,TCACTG
+              </FormHelperText>
+            }
+          </VStack>
+          <Button color={"primary"} bgColor={"bg.500"} px={6} type="submit">Es mutante?</Button>
+        </FormControl>
+        {
+          isMutant && <Alert
+            status='success'
+            variant='subtle'
+            flexDirection='column'
+            alignItems='center'
+            justifyContent='center'
+            textAlign='center'
+            height='200px'
+            bgColor={"#8fa800"}
+          >
+            <AlertIcon boxSize='40px' mr={0} color={"white"} />
+            <AlertTitle mt={4} mb={1} fontSize='lg' color={"bg.500"}>
+              Estudiante preparado para luchar!
+            </AlertTitle>
+            <AlertDescription maxWidth='sm' color={"bg.500"}>
+              La secuencia de ADN coincide con más de 4 letras iguales en sus diferentes formar (oblicua, horizontal o vertical)
+            </AlertDescription>
+          </Alert>
+        }
+        <ConsultStat />
       </Container>
     </Box>
   )
--- src/hooks/useMutant.ts
+++ src/hooks/useMutant.ts
@@ -3,19 +3,27 @@ import { useCallback, useRef, useState } from 'react';
 
 export function useMutant({ ADN }: { ADN: string }) {
   const [isMutant, setIsMutant] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const previousADN = useRef(ADN)
-
+  function removeMessage() {
+    setTimeout(() => {
+      setIsMutant(false)
+    }, 5000);
+  }
   const validMutant = useCallback(async ({ ADN }: { ADN: string }) => {
     if (ADN === previousADN.current) return
 
     try {
+      setIsLoading(true)
       previousADN.current = ADN
       const response = await axios.post('/mutant', { "dna": ADN.split(",") })
       setIsMutant(response.status === 200 ? true : false)
+      removeMessage()
     } catch (error) {
       console.log(error)
+    } finally {
+      setIsLoading(false)
     }
-
   }, [])
-  return { validMutant, isMutant }
+  return { validMutant, isMutant, isLoading }
 }

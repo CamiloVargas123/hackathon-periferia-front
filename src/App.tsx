24167da83d0d58bfd17cf510efd952ede3854fce
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-08 21:16:51 -0500

    create feedback mutant with sweetalert2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, Container, FormControl, FormErrorMessage, FormHelperText, Input, VStack } from "@chakra-ui/react"
+import { Box, Button, Container, FormControl, FormErrorMessage, FormHelperText, Input, VStack } from "@chakra-ui/react"
 import { useEffect, useRef, useState } from "react"
+import Swal from 'sweetalert2'
 import ConsultStat from "./components/ConsultStat"
 import { useMutant } from "./hooks/useMutant"
 
+const showMessage = (isMutant: boolean) => {
+  Swal.fire({
+    title: isMutant ? "Estudiante preparado para luchar!" : "Este estudiante no tiene mutaciones",
+    text: isMutant ? "La secuencia de ADN coincide con más de 4 letras iguales en sus diferentes formar (oblicua, horizontal o vertical)" :
+      "Su secuencia de ADN no coincide de ninguna mutacion",
+    icon: isMutant ? 'success' : "warning",
+    position: 'top',
+    showConfirmButton: false,
+    backdrop: "transparent",
+    timer: 4000
+  });
+};
+
 function App() {
   const { ADN, updateADN, error } = useValid()
-  const { validMutant, isMutant, isLoading } = useMutant({ ADN })
+  const { validMutant, isLoading } = useMutant({ ADN, showMessage })
 
   const handleSubmit = (e: React.FormEvent<HTMLDivElement>) => {
     e.preventDefault()
@@ -20,7 +34,7 @@ function App() {
   return (
     <Box as="main" h={"max-content"}>
       <Container maxW='2xl' justifyContent={"space-between"} centerContent paddingBlock={10} gap={10}>
-        <FormControl as="form" display={"flex"} alignItems={"start"} gap={10} isInvalid={!!error} onSubmit={handleSubmit} isDisabled={isLoading || isMutant}>
+        <FormControl as="form" display={"flex"} alignItems={"start"} gap={10} isInvalid={!!error} onSubmit={handleSubmit} isDisabled={isLoading}>
           <VStack gap={0} w="100%" alignItems={"flex-start"}>
             <Input placeholder="Ingrese la secuencia de ADN" value={ADN} onChange={handleChange} />
             {error ? <FormErrorMessage>{error}</FormErrorMessage> :
@@ -31,34 +45,12 @@ function App() {
           </VStack>
           <Button color={"primary"} bgColor={"bg.500"} px={6} type="submit">Es mutante?</Button>
         </FormControl>
-        {
-          isMutant && <Alert
-            status='success'
-            variant='subtle'
-            flexDirection='column'
-            alignItems='center'
-            justifyContent='center'
-            textAlign='center'
-            height='200px'
-            bgColor={"#8fa800"}
-          >
-            <AlertIcon boxSize='40px' mr={0} color={"white"} />
-            <AlertTitle mt={4} mb={1} fontSize='lg' color={"bg.500"}>
-              Estudiante preparado para luchar!
-            </AlertTitle>
-            <AlertDescription maxWidth='sm' color={"bg.500"}>
-              La secuencia de ADN coincide con más de 4 letras iguales en sus diferentes formar (oblicua, horizontal o vertical)
-            </AlertDescription>
-          </Alert>
-        }
         <ConsultStat />
       </Container>
     </Box>
   )
 }
-
 export default App
-
 
 function useValid() {
   const [ADN, updateADN] = useState<string>('')
@@ -70,12 +62,10 @@ function useValid() {
       isFirstInput.current = ADN === ''
       return
     }
-
     if (ADN === '') {
       setError('No se puede buscar una secuencia de ADN vacía')
       return
     }
-
     setError(undefined)
   }, [ADN])
 
--- src/hooks/useMutant.ts
+++ src/hooks/useMutant.ts
@@ -1,29 +1,25 @@
 import axios from "axios";
 import { useCallback, useRef, useState } from 'react';
 
-export function useMutant({ ADN }: { ADN: string }) {
+export function useMutant({ ADN, showMessage }: { ADN: string, showMessage: (_: boolean) => void }) {
   const [isMutant, setIsMutant] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const previousADN = useRef(ADN)
-  function removeMessage() {
-    setTimeout(() => {
-      setIsMutant(false)
-    }, 5000);
-  }
+
   const validMutant = useCallback(async ({ ADN }: { ADN: string }) => {
     if (ADN === previousADN.current) return
-
     try {
       setIsLoading(true)
       previousADN.current = ADN
       const response = await axios.post('/mutant', { "dna": ADN.split(",") })
       setIsMutant(response.status === 200 ? true : false)
-      removeMessage()
+      showMessage(true)
     } catch (error) {
-      console.log(error)
+      showMessage(false)
+      console.error(error)
     } finally {
       setIsLoading(false)
     }
-  }, [])
+  }, [showMessage])
   return { validMutant, isMutant, isLoading }
 }

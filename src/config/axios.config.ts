2797ba0a3731685c35aa56a3f6932251ba3067ee
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-11 21:00:03 -0500

    add url enviroment

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_PATH = 'https://hackathon-periferia.onrender.com';
+const BASE_PATH = import.meta.env.VITE_BACKEND_URL;
 
 axios.defaults.baseURL = BASE_PATH;
 axios.defaults.headers.common['Accept'] = 'application/json';

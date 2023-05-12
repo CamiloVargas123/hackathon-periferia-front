6d8c602e16eda2b70d0ec1b18ec996f358e198b3
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-12 13:54:34 -0500

    upgrade when type letter and focus

--- src/components/TableInput.tsx
+++ src/components/TableInput.tsx
@@ -1,11 +1,11 @@
 import { Alert, AlertIcon, Button, HStack, Input, Text, VStack } from '@chakra-ui/react'
-import { useEffect, useMemo } from 'react'
+import { useCallback, useEffect, useMemo, useRef } from 'react'
 import { useForm } from 'react-hook-form'
 import type { TableSize } from '../App'
 import { useMutant } from '../hooks/useMutant'
 import Swal from 'sweetalert2'
 
-const acepptKey = ["A", "T", "C", "G", ""]
+const acepptKey = ["A", "T", "C", "G"]
 interface NextFocus {
   tableSize: TableSize
   rowIndex: number
@@ -30,25 +30,30 @@ function nextFocus({ tableSize, rowIndex, colIndex }: NextFocus): NextFocusResul
   }
   return result
 }
-const showMessage = (isMutant: boolean) => {
-  Swal.fire({
-    title: isMutant ? "Estudiante preparado para luchar!" : "Este estudiante no tiene mutaciones",
-    text: isMutant ? "La secuencia de ADN coincide con más de 4 letras iguales en sus diferentes formar (oblicua, horizontal o vertical)" :
-      "Su secuencia de ADN no coincide de ninguna mutacion",
-    icon: isMutant ? 'success' : "warning",
-    position: 'center',
-    showConfirmButton: false,
-    backdrop: "transparent",
-    timer: 4000
-  });
-};
+
 
 interface TableInputProps extends TableSize {
   resetForm?: boolean
 }
 export default function TableInput({ rows, columns, resetForm }: TableInputProps) {
   const table = useMemo(() => Array.from({ length: rows }, () => Array.from({ length: columns }, () => "")), [rows, columns])
-  const { register, handleSubmit, formState: { errors }, setValue, setFocus, clearErrors, reset } = useForm({ mode: "onSubmit" });
+  const { register, handleSubmit, formState: { errors }, setValue, setFocus, reset } = useForm({ mode: "onSubmit" });
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const showMessage = useCallback(
+    (isMutant: boolean) => {
+      Swal.fire({
+        title: isMutant ? "Estudiante preparado para luchar!" : "Este estudiante no tiene mutaciones",
+        text: isMutant ? "Su secuencia de ADN indica mas de una mutación" :
+          "Su secuencia de ADN no coincide de ninguna mutación",
+        icon: isMutant ? 'success' : "warning",
+        position: 'center',
+        showConfirmButton: false,
+        timer: 4000
+      }).then(() => setFocus('0,0'));
+    },
+    [setFocus],
+  )
   const { validMutant, isLoading } = useMutant({ showMessage })
 
   const onSubmit = (data: any) => {
@@ -59,6 +64,7 @@ export default function TableInput({ rows, columns, resetForm }: TableInputProps
     }
     const transform = table.map(e => e.join(""))
     validMutant({ ADN: transform })
+    reset()
   }
 
   useEffect(() => {
@@ -67,10 +73,7 @@ export default function TableInput({ rows, columns, resetForm }: TableInputProps
 
   useEffect(() => {
     reset()
-    clearErrors();
-  }, [resetForm, clearErrors, reset])
-
-
+  }, [resetForm, reset])
 
   return (
     <VStack as="form" onSubmit={handleSubmit(onSubmit)} gap={2}>
@@ -86,8 +89,6 @@ export default function TableInput({ rows, columns, resetForm }: TableInputProps
                 key={colIndex + "," + colIndex}
                 type="text"
                 w="80px"
-                minLength={1}
-                maxLength={1}
                 textTransform={"uppercase"}
                 textAlign={"center"}
                 bgColor={errors[`${rowIndex},${colIndex}`] ? "rgba(255, 0, 0, 0.3)" : "initial"}
@@ -97,18 +98,21 @@ export default function TableInput({ rows, columns, resetForm }: TableInputProps
                   onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                     const value = e.target.value.toUpperCase()
                     if (!acepptKey.includes(value)) return setValue(`${rowIndex},${colIndex}`, "")
+                    setValue(`${rowIndex},${colIndex}`, value)
+                    if (rowIndex === rows - 1 && colIndex === columns - 1) return buttonRef.current?.focus()
                     if (value !== "") {
                       const nextValue = nextFocus({ tableSize: { rows, columns }, rowIndex, colIndex })
                       return setFocus(`${nextValue.nextRow},${nextValue.nextColumn}`)
                     }
-                  }
+                  },
+                  value: ""
                 })}
               />
             ))}
           </HStack>
         ))}
       </VStack>
-      <Button w="100%" color={"primary"} bgColor={"bg.500"} px={6} type="submit" isLoading={isLoading}>Es Mutante?</Button>
+      <Button w="min-content" color={"primary"} bgColor={"bg.500"} px={6} type="submit" isLoading={isLoading} ref={buttonRef}>Es Mutante?</Button>
       {
         Object.entries(errors).length > 0 && <Alert status='error' borderRadius={"md"}>
           <AlertIcon />

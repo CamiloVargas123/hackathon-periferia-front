a812b333b207c5c24786d1917ee1efd6e57a3c32
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-20 13:02:10 -0500

    install and configure react-query

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -5,3 +5,4 @@ const BASE_PATH = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.baseURL = BASE_PATH;
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.timeout = 5000
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from './theme.ts'
 import './config/axios.config'
+import theme from './theme.ts'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
